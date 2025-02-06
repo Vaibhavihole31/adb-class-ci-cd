3d3e8f2cb3e6542b4dc0c905149a1411368a3dbd
Vaibhavihole31 <itspinki05@gmail.com>
2025-02-06 19:15:52 +0530

    basic setup

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.get("/", async(req, res)=>{
+    res.send(`<h1>All Good </h1>`)
+})
+
+app.get("/test", (req, res)=>{
+    res.send("<h1>Hey I am Working....</h1>")
+})
+
+app.get("/health", (req, res)=>{
+    res.send("<h1>Awesome....</h1>")
+})
+
+const PORT = 5002;
+app.listen(PORT, ()=>{
+    console.log(`Server is listening on ${PORT}`)
+});
